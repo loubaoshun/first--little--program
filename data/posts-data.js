c65ede85d2bc425074bc416d818b2dd781f869d7
lbs <1791268140@qq.com>
2019-01-09 13:10:58 +0800

    first

---
+++ data/posts-data.js
@@ -0,0 +1,121 @@
+var local_database = [
+    {
+        date: "19日 12月 2018年",
+        title: "第一次遇见",
+        imgSrc: "/images/post/crab.jpg",
+        avatar: "/images/avatar/1.png",
+    content: "有些事，我们明知道是错的，也要去坚持，因为不甘心；有些人，我们明知道是爱的，也要去放弃，因为没结局；有时候，我们明知道没路了，却还在前行，因为习惯了。'",
+        reading: "112",
+        collection: "96",
+        headImgSrc: "/images/post/crab.jpg",
+        author: "lbs",
+        dateTime: "24小时前",
+    detail: "我们的爱情，我从一开始就知道了结局，所以我并不会后悔，我只是害怕，害怕会不会有一天，我会突然忘记我为什么要站在这里等待'",
+        postId: 0,
+        music: {
+          url: "http://fs.w.kugou.com/201901090151/5b595798684da79367f13854ee268330/G108/M09/0B/17/rA0DAFwvV3qAO_bLAD62hSd89xQ163.mp3",
+          title: "胡夏",
+          coverImg: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=210871800,2429543&fm=27&gp=0.jpg"
+        }
+    },
+    {
+        title: "相识",
+      content: "只有毅力才会使我们成功，而毅力的来源又在于毫不动摇，坚决采取为达到成功所需要的手段",
+        imgSrc: "/images/post/bl.jpg",
+        reading: 62,
+      detail: "爱，不是长久地对视，而是，总能朝同一个方向望去",
+        collection: 92,
+        dateTime: "24小时前",
+        headImgSrc: "/images/post/bl.jpg",
+        author: "lbs",
+        date: "20日 12月 2018年",
+        avatar: "/images/avatar/1.png",
+        postId: 1,
+        music: {
+          url: "http://fs.w.kugou.com/201901090151/5b595798684da79367f13854ee268330/G108/M09/0B/17/rA0DAFwvV3qAO_bLAD62hSd89xQ163.mp3",
+          title: "胡夏",
+          coverImg: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=210871800,2429543&fm=27&gp=0.jpg"
+        }
+    },
+    {
+        //按住alt + shift + F 可以格式化代码样式
+        title: "渐渐的就爱上了你",
+      content: "你的生命就像你的家。因为你的坚持，让我走进你的内心。你不要忘了，在你身边还有个我。",
+      detail: " 爱可以是一瞬间的事情，也可以是一辈子的事情。有没有一个肩膀，可以倚靠一辈子都有安全感；有没有一场拥抱，紧紧的让两个人再也不分开；有没有一种约定，是相约每一个来生都要和你相遇；有没有一个人，是你用尽了一生力气还舍不得将它遗忘",
+        imgSrc: "/images/post/sls.jpg",
+        headImgSrc: "/images/post/sls.jpg",
+        reading: 62,
+        collection: 92,
+        author: "lbs",
+        date: "20日 12月 2018年",
+        dateTime: "三天前",
+        avatar: "/images/avatar/3.png",
+        postId: 2,
+        music: {
+          url: "http://fs.w.kugou.com/201901090151/5b595798684da79367f13854ee268330/G108/M09/0B/17/rA0DAFwvV3qAO_bLAD62hSd89xQ163.mp3",
+          title: "胡夏",
+          coverImg: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=210871800,2429543&fm=27&gp=0.jpg"
+        }
+    },
+    {
+        title: "emmmm",
+      content: "一句我等你，不知道需要多大的勇气。它远比我爱你三个字，更需要勇气。不是每个人你都愿意等待，也不是所有人都值得你去等待。一句我等你，包含了很多的无奈，心酸，苦涩。或许是爱不到，或许是不能爱，无论怎样，我等你这个承诺，远比我爱你更动听。可是有多少的爱情经得起等待……",
+        imgSrc: "/images/wx.jpg",
+        reading: 102,
+      detail: " 不要认为后面还有更好的，因为现在拥有的就是最好的；不要因为距离太远而放弃，爱情是可以和你一起坐火车的。一生只谈一次恋爱是最好的，经历的太多了，会麻木；分离多了，会习惯；换恋人多了，会比较；到最后，你不会再相信爱情。其实对于爱情，越单纯越幸福。",
+        collection: 92,
+        dateTime: "24小时前",
+        headImgSrc: "/images/wx.jpg",
+        author: "lbs",
+        date: "21日 12月 2018年",
+        avatar: "/images/avatar/5.png",
+        postId: 3,
+        music: {
+          url: "http://fs.w.kugou.com/201901090151/5b595798684da79367f13854ee268330/G108/M09/0B/17/rA0DAFwvV3qAO_bLAD62hSd89xQ163.mp3",
+          title: "胡夏",
+          coverImg: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=210871800,2429543&fm=27&gp=0.jpg"
+        }
+    },
+    {
+        title: "啊哈哈哈哈",
+      content: "如果幸福不在巴黎，就一定在别处。可是我的世界已没有巴黎，只有你。2.时间没有等我，是你忘了带我走。3.你喜欢我，因为我是个不错的人，但你爱他，哪怕他是个错的人。4.我已经暗恋他很久了，如果我不走到他的前面，他永远不会看见我。5.岁岁长相伴，白头不相离。",
+        imgSrc: "/images/vr.jpg",
+        reading: 102,
+      detail: " 喜欢一个人，跟他在一起的时候，时间老不够用，可是不跟他在一起的时候，时间却总用不完。喜欢一个人，跟他在一起的时候，总有说不完的话题，可是不跟他在一起的时候，却懒的说半句话。喜欢一个人，跟他在一起的时候，表情会多的不够用，可是不跟他在一起的时候表情通常只有一种：想念……",
+        collection: 26,
+        dateTime: "24小时前",
+        headImgSrc: "/images/vr.jpg",
+        author: "lbs",
+      date: "21日 12月 2018年",
+        avatar: "../../../images/avatar/3.png",
+        postId: 4,
+        music: {
+          url: "http://fs.w.kugou.com/201901090151/5b595798684da79367f13854ee268330/G108/M09/0B/17/rA0DAFwvV3qAO_bLAD62hSd89xQ163.mp3",
+          title: "胡夏",
+          coverImg: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=210871800,2429543&fm=27&gp=0.jpg"
+        }
+    },
+    {
+        title: "我想对你说。。。。",
+      content: " 在自己面前，应该一直留有一个地方，独自留在那里。然后去爱。不知道是什么，不知道是谁，不知道如何去爱，也不知道可以爱多久。只是等待一次爱情，也许永远都没有人。可是，这种等待，就是爱情本身。",
+        imgSrc: "/images/iqiyi.jpg",
+        reading: 96,
+      detail: "爱一个人，恋一座城，一场震撼，是那样自然，那样执着，此刻静静的倾听心语，默默的触及灵魂深处，往昔的悲喜，曾经的难分难舍，不经意间已成过去，你的呼吸，你的影，你的手，却在眼前不断浮现，就这样，每日每天，每时每刻，心甘情愿陶醉有你的画面中，虽然会忍不住悲伤，虽然会忍不住流泪，我却宁愿在岁月中酝酿一份关于你我的片段。",
+        collection: 26,
+        dateTime: "21小时前",
+        headImgSrc: "/images/iqiyi.jpg",
+        author: "lbs",
+        date: "22日 12月 2018年",
+        avatar: "../../../images/avatar/5.png",
+        postId: 5,
+        music: {
+          url: "http://fs.w.kugou.com/201901090151/5b595798684da79367f13854ee268330/G108/M09/0B/17/rA0DAFwvV3qAO_bLAD62hSd89xQ163.mp3",
+          title: "胡夏",
+          coverImg: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=210871800,2429543&fm=27&gp=0.jpg"
+        }
+    },
+]
+
+module.exports = {
+    postList: local_database
+}
